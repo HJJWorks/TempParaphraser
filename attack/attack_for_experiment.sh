77cf4da2586b2007c542a8d59f1b8b2ebac33d05
huangjunjie877 <877207583@qq.com>
2025-10-28 00:40:18 +0000

    fix bug

--- attack/attack_for_experiment.sh
+++ attack/attack_for_experiment.sh
@@ -1,15 +1,15 @@
 #for Times in $(seq 1 8) 
 #do
-Times=1.2
+Times=7
   #for Temp in $(seq 0.5 0.1 1.5)  # temperature:0.5-1.5
   #do
-  Temp=7
-    python /TempParaphraser/attack/paraphraser.py \
+  Temp=1.2
+    python attack/paraphraser.py \
       --eval_model_name hc3 \
       --rewrite_times $Times \
       --temperature $Temp \
-      --input_path /data/HC3/test_rnd10k.jsonl \
-      --save_path /TempParaphraser/result/llama3.2-1b/n$Times-T$Temp-hc3.json \
+      --input_path test_data/test_rnd10k.jsonl \
+      --save_path test_data/n$Times-T$Temp-hc3.json \
       --api http://0.0.0.0:10001/v1
   #done
 #done
